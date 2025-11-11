2c4d71c16beaae88a04402b0d309a2218edd8e9a
Paulo <pauloricardosn10@gmail.com>
2025-11-11 16:31:14 -0300

    Configuração inicial do projeto

---
+++ src/test/java/com/projects/personal_finance_api/PersonalFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.personal_finance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
